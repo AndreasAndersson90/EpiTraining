dd652cfa7bc4ca31bad8c1dda187bb3ef9b435cc
Andreas Andersson <Andreas.andersson90@live.se>
2017-10-06 15:31:59 +0200

    B1
    S62 - optionall

---
+++ EpiserverTraining/AlloyTrainingFundamentals/Models/Pages/StartPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTrainingFundamentals.Models.Pages
+{
+    [ContentType(
+        DisplayName = "Start", 
+        GUID = "07347341-dc09-4b38-8867-5fbb5bbe8292",
+        Description = "The startpage",
+        GroupName = SiteGroupNames.Specialized)]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "If the Heading is not set, the page falls back to showing the Name.",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
---
+++ EpiserverTraining/AlloyTrainingFundamentals/SiteGroupNames.cs
@@ -0,0 +1,9 @@
+﻿namespace AlloyTrainingFundamentals
+{
+    public class SiteGroupNames
+    {
+        public const string Specialized = "Specialized";
+        public const string Common = "Common";
+        public const string News = "News";
+    }
+}
